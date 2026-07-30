a5c4002fc15b5abb8c9ad16076e282894783b163
ganbat24 <s.ganbat24@gmail.com>
2026-07-30 02:50:58 +0100

    style: switch import-order-style to cryptography to match CI

--- src/amr_description/launch/view.launch.py
+++ src/amr_description/launch/view.launch.py
@@ -1,13 +1,13 @@
 # Copyright 2026 Ganbat Selenge
 #
-# Licensed under the Apache License, Version 2.0 (the 'License');
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -18,8 +18,10 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+
 import xacro
 
 
--- src/amr_description/test/test_rsp_launch.py
+++ src/amr_description/test/test_rsp_launch.py
@@ -1,13 +1,13 @@
 # Copyright 2026 Ganbat Selenge
 #
-# Licensed under the Apache License, Version 2.0 (the 'License');
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -17,15 +17,22 @@ import os
 import unittest
 
 from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.substitutions import FindPackageShare
+
 import launch_testing
 import launch_testing.actions
+
 from launch_testing_ros import WaitForTopics
+
 from sensor_msgs.msg import JointState
+
 from tf2_msgs.msg import TFMessage
+
 import xacro
 
 
